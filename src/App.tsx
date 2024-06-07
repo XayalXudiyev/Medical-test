5668416229abbd72622bef6c191a91f603cd389c
XayalXudiyev <xeyalxeyal068@gmail.com>
2024-06-06 23:32:35 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="bg-red-300">
+      <Button className="">
+        salam
+      </Button>
+    </div>
+  )
+}
+
+export default App
